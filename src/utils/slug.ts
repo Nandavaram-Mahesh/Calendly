0a3d2fdf5adcc888dad75ebddde0736c319b0ea7
Nandavaram-Mahesh <mahesh.nandavaram96@gmail.com>
2026-07-03 07:04:24 +0530

    feat: Added Event Slug generation logic

--- src/utils/slug.ts
+++ src/utils/slug.ts
@@ -1,4 +1,20 @@
-import { nanoid } from "nanoid";
+import { customAlphabet,nanoid } from "nanoid";
+
 export function generateUserSlug(){
-    return nanoid(6);
+    return nanoid(8);
+}
+
+export function generateEventSlug(text:string){
+    const nanoid = customAlphabet("0123456789abcdefghijklmnopqrstuvwxyz", 8);
+    
+    const slug = text
+    .toLowerCase()
+    .trim()
+    .replace(/['"]/g, "") // Remove quotes
+    .replace(/[^a-z0-9\s]+/g, "") // Replace non-alphanumeric chars with ""
+    .replace(/\s+/g, "-") // Replace spaces with -
+    .replace(/^-+|-+$/g, "") // Remove leading/trailing -
+    .replace(/-+/g, "-"); // Collapse multiple -
+
+    return `${slug}-${nanoid()}`
 }
