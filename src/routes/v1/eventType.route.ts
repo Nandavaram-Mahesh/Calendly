37f629a1c75565946b2e6f372bc7de7cde4d9335
Nandavaram-Mahesh <mahesh.nandavaram96@gmail.com>
2026-07-04 15:28:09 +0530

    Added UserId middleware to every route

--- src/routes/v1/eventType.route.ts
+++ src/routes/v1/eventType.route.ts
@@ -2,9 +2,12 @@ import {Router} from 'express';
 import { addEventType, fetchAllEventTypes, fetchEventTypeById, getPublicEventType, modifyEventType, removeEventType } from '../../controllers/index.js';
 import { createEventTypeSchema, updateEventTypeSchema } from '../../dtos/index.js';
 import { validate } from '../../middlewares/index.js';
+import { requireUserId } from '../../middlewares/requireUserId.middleware.js';
 
 
 const eventTypeRouter:Router = Router();
+
+eventTypeRouter.use(requireUserId);
 
 eventTypeRouter.get('/',fetchAllEventTypes);
 
