f567bd46505d3db8bf81a58475c93e2724a0d902
Nandavaram-Mahesh <mahesh.nandavaram96@gmail.com>
2026-07-15 20:16:06 +0530

    Add validation middleware to the availability routes

--- src/routes/v1/availability.route.ts
+++ src/routes/v1/availability.route.ts
@@ -1,15 +1,18 @@
 import { Router } from 'express';
 import { fetchRules, createRule, updateRule, removeRule, fetchExceptions, createException, updateException, removeException } from '../../controllers/availability.controller.js';
+import { createAvailabilityExceptionSchema, createAvailabilityRuleSchema, updateAvailabilityExceptionSchema, updateAvailabilityRuleSchema } from '../../dtos/index.js';
+import { validate } from '../../middlewares/validate.middleware.js';
 
 const availabilityRouter = Router();
 
 availabilityRouter.get('/rules', fetchRules);
-availabilityRouter.post('/rules', createRule);
-availabilityRouter.patch('/rules/:id', updateRule);
+availabilityRouter.post('/rules',validate(createAvailabilityRuleSchema) ,createRule);
+availabilityRouter.patch('/rules/:id',validate(updateAvailabilityRuleSchema), updateRule);
 availabilityRouter.delete('/rules/:id', removeRule);
+
 availabilityRouter.get('/exceptions', fetchExceptions);
-availabilityRouter.post('/exceptions', createException);
-availabilityRouter.patch('/exceptions/:id', updateException);
+availabilityRouter.post('/exceptions',validate(createAvailabilityExceptionSchema),createException);
+availabilityRouter.patch('/exceptions/:id',validate(updateAvailabilityExceptionSchema), updateException);
 availabilityRouter.delete('/exceptions/:id', removeException);
 
 export { availabilityRouter };
