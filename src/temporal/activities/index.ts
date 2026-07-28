840bcb03c1fae00b5136a574fe1fc0938c0364de
Nandavaram-Mahesh <mahesh.nandavaram96@gmail.com>
2026-07-28 18:35:51 +0530

    Added sendbookingConfirmationEmail activity

--- src/temporal/activities/index.ts
+++ src/temporal/activities/index.ts
@@ -1,5 +1,10 @@
+import { sendBookingConfirmationEmail } from "../../mailer/booking.mailer.js";
 import { regenerateHostSlots as runSlotGeneration, RegenerateHostSlotsInput } from "../../services/slot.service.js";
 
 export async function regenerateHostSlotsActivity(input:RegenerateHostSlotsInput){
     await runSlotGeneration(input); 
+}
+
+export async function sendBookingConfirmationEmailActivity(bookingId: number) {
+    await sendBookingConfirmationEmail(bookingId);
 }
